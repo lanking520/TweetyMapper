2d57a66133445fa104eeabc103391235f23e995d
lanking520 <lanking520@gmail.com>
2017-03-08 17:57:20 -0500

    Minor Error fix

--- AngularJS/js/mymap.js
+++ AngularJS/js/mymap.js
@@ -2,14 +2,23 @@
 /// </// <reference path="markers.js" />
 /// </// <reference path="js/markerclusterer.js" />
 var app = angular.module('ngMap')
-app.controller('MyController', function($http, $interval, NgMap) {
+app.controller('MyController', function($http, $scope, $interval, NgMap) {
     var vm = this;
-    vm.dynMarkers = [];
-    NgMap.getMap().then(function(map) {
+    $scope.init = function(){
+      $scope.words = {}
+    };
+    $scope.searchword = function(){
+      vm.dynMarkers = [];
+      $scope.words[$scope.word] = true
+      NgMap.getMap().then(function(map) {
       for (var i=0; i<1000; i++) {
         var latLng = new google.maps.LatLng(markers[i].position[0], markers[i].position[1]);
         vm.dynMarkers.push(new google.maps.Marker({position:latLng}));
       }
       vm.markerClusterer = new MarkerClusterer(map, vm.dynMarkers, {});
     });
+    $scope.deleteword = function(word){
+      delete $scope.words[word];
+    };
+    };
   });
