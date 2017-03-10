01a1760583c9c2aa21e4cedb1e934b9cca1d638a
lanking520 <lanking520@gmail.com>
2017-03-10 15:43:12 -0500

    Minor Error Fix

--- AngularJS/js/mymap.js
+++ AngularJS/js/mymap.js
@@ -6,19 +6,22 @@ app.controller('MyController', function($http, $scope, $interval, NgMap) {
     var vm = this;
     $scope.init = function(){
       $scope.words = {}
+      NgMap.getMap().then(function(map) {
+        vm.map = map;
+      });
+      vm.markerClusterer = new MarkerClusterer(vm.map, [], {});
     };
     $scope.searchword = function(){
+      $scope.words[$scope.word] = true;
+      vm.markerClusterer.clearMarkers();
       vm.dynMarkers = [];
-      $scope.words[$scope.word] = true
-      NgMap.getMap().then(function(map) {
       for (var i=0; i<markers.length; i++) {
         var latLng = new google.maps.LatLng(markers[i].position[1], markers[i].position[0]);
         vm.dynMarkers.push(new google.maps.Marker({position:latLng}));
       }
-      vm.markerClusterer = new MarkerClusterer(map, vm.dynMarkers, {});
-    });
+      vm.markerClusterer = new MarkerClusterer(vm.map, vm.dynMarkers, {});
+    };
     $scope.deleteword = function(word){
       delete $scope.words[word];
-    };
-    };
-  });
+      };
+});
