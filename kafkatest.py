2e4e0f301414c74dbc8b0f22a034213a2d89252f
lanking520 <lanking520@gmail.com>
2017-04-01 17:46:13 -0400

    Problem solved

--- kafkatest.py
+++ kafkatest.py
@@ -2,6 +2,9 @@ import threading, logging, time
 from kafka import KafkaConsumer
 from kafka import TopicPartition
 
-consumer = KafkaConsumer("twitterstream",max_poll_records=10,auto_offset_reset='earliest', enable_auto_commit=False)
+consumer = KafkaConsumer("twitterstream", group_id='my-group', max_poll_records=1, auto_offset_reset='earliest', enable_auto_commit=True)
+
 msg = consumer.poll(timeout_ms=2000)
+print msg
 print len(msg.values()[0])
+consumer.close(autocommit=True)
