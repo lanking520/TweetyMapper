7e475f695bb7a93f658307e37dd87ce301b7c859
lanking520 <lanking520@gmail.com>
2017-04-09 13:56:39 -0400

    fix flask

--- Flask/static/js/mymap.js
+++ Flask/static/js/mymap.js
@@ -1,6 +1,6 @@
 /// </// <reference path="markers.js" />
 /// </// <reference path="js/markerclusterer.js" />
-var preUrl = "http://127.0.0.1:5000"
+var preUrl = ""
 var app = angular.module('myapp',['ngMap','ngAnimate', 'ngSanitize', 'ui.bootstrap'])
 app.controller('MyController', MapCtrl);
 
@@ -19,11 +19,12 @@ function Markerer (latLng, mmmap, tweet, sentiment){
 }
 
 function updater(vm, $scope, $http, filters = null){
+  vm.updateClusterer.clearMarkers();
   vm.markerClusterer.clearMarkers();
   vm.dynMarkers = [];
   obj = {"keyword":Object.keys($scope.words),"filter":filters};
   $http({
-            url: "/search",
+            url: preUrl+"/search",
             method: "POST",
             data: obj
         }).then(function (success){
@@ -39,30 +40,35 @@ function updater(vm, $scope, $http, filters = null){
 function MapCtrl($http, $scope, $interval, NgMap){
     var vm = this;
     var updatetweet =function(){
-      if(Object.keys($scope.words).length === 0){
-       vm.dynMarkers = [];
+      if(vm.updateMarkers.length > 0){
+        $scope.hideupdate = false;
+        $scope.numofupdates = vm.updateMarkers.length;
+      }
+        console.log(vm.updateMarkers);
        $http({
-            url: "/updates",
+            url: preUrl+"/updates",
             method: "GET"
         }).then(function(success){
           markers = success["data"]["result"];
           if(markers.length > 0){
             for (var i=0; i<markers.length; i++) {
             var latLng = new google.maps.LatLng(markers[i].coordinates[1], markers[i].coordinates[0]);
-            vm.dynMarkers.push(Markerer(latLng, vm.map, markers[i].text,$scope.sentimentlib[markers[i].sentiment]));
+            vm.updateMarkers.push(Markerer(latLng, vm.map, markers[i].text,$scope.sentimentlib[markers[i].sentiment]));
             }
-            vm.markerClusterer = new MarkerClusterer(vm.map, vm.dynMarkers, {});
+            //vm.markerClusterer = new MarkerClusterer(vm.map, vm.dynMarkers, {});
           };},function (error){console.log(error)});
       }
-    }
     $scope.init = function(){
       $scope.oldwords = ["music", "food", "sport", "show", "movie", "car", "commercial", "party", "war", "hello"];
       $scope.words = {};
       NgMap.getMap().then(function(map) {
         vm.map = map;
       });
+      $scope.hideupdate = true;
       vm.markerClusterer = new MarkerClusterer(vm.map, [], {});
+      vm.updateClusterer = new MarkerClusterer(vm.map, [], {});
       $scope.sentimentlib = {"positive":"green","neutral":"blue","negative":"red"};
+      vm.updateMarkers = [];
       $interval(updatetweet, 2000);
     };
     $scope.searchword = function(){
@@ -98,7 +104,15 @@ function MapCtrl($http, $scope, $interval, NgMap){
         updater(vm, $scope, $http, Latlgn);
       }
     };
+
+    $scope.fetchupdate = function() {
+      $scope.words = {};
+      vm.updateClusterer.clearMarkers();
+      vm.markerClusterer.clearMarkers();
+      vm.updateClusterer = new MarkerClusterer(vm.map, vm.updateMarkers, {});
+      $scope.hideupdate = true;
+      $scope.numofupdates = 0;
+      vm.updateMarkers = [];
+    };
+
 };
-
-
-
